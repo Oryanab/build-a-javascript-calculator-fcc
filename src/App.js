255afe070204b045151281946ddb0fa51ef9e32b
Oryanab <86202122+Oryanab@users.noreply.github.com>
2021-11-29 18:31:07 +0200

    update 3.0

--- src/App.js
+++ src/App.js
@@ -1,16 +1,75 @@
-import react, { useState, useRef } from "react";
+import react, { useState, useRef, useEffect } from "react";
 
 function App() {
+  const delButton = useRef(null);
+  const acButton = useRef(null);
+  const previousDiv = useRef(null);
+  const currentDiv = useRef(null);
+
+  const [previousOperand, setPreviousOperand] = useState(0);
+  const [currentOperand, setCurrentOperand] = useState(null);
+  const [mathOperation, setMathOperation] = useState(null);
+
+  window.addEventListener("load", (e) => {
+    setCurrentOperand(0);
+    setPreviousOperand(0);
+  });
+
+  useEffect(() => {}, [currentOperand]);
+  useEffect(() => {}, [previousOperand]);
+  useEffect(() => {}, [mathOperation]);
+
+  let initialNumberSting;
+  document.querySelectorAll("button").forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.stopImmediatePropagation();
+      if (
+        parseFloat(e.target.value) ||
+        parseFloat(e.target.value) === 0 ||
+        e.target.value === "."
+      ) {
+        initialNumberSting += e.target.value;
+        setCurrentOperand(parseFloat(initialNumberSting.split("undefined")[1]));
+      } else if (
+        e.target.value === "+" ||
+        e.target.value === "-" ||
+        e.target.value === "x" ||
+        e.target.value === "÷"
+      ) {
+        if (previousDiv.current.innerText === "0") {
+          setPreviousOperand(parseFloat(currentDiv.current.innerText));
+          setMathOperation(e.target.value);
+          initialNumberSting = "undefined";
+          setCurrentOperand(0);
+        } else {
+          if (mathOperation === "+") {
+            let score =
+              previousOperand + parseFloat(currentDiv.current.innerText);
+            console.log(score);
+            setPreviousOperand(score);
+            setMathOperation(e.target.value);
+            initialNumberSting = "undefined";
+            setCurrentOperand(0);
+          }
+        }
+      }
+    });
+  });
+
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div id="display" className="current-operand"></div>
+        <div ref={previousDiv} className="previous-operand">
+          {previousOperand}
+        </div>
+        <div ref={currentDiv} id="display" className="current-operand">
+          {currentOperand}
+        </div>
       </div>
-      <button id="clear" className="span-two">
+      <button ref={acButton} id="clear" className="span-two">
         AC
       </button>
-      <button>DEL</button>
+      <button ref={delButton}>DEL</button>
       <button value="÷" id="divide">
         ÷
       </button>
